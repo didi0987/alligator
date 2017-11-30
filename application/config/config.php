fb9defa5f554d100dba60518832f15bb4419805b
chenxiaowang <didi0987@gmail.com>
2017-11-30 01:14:52 -0600

    add res folder

--- application/config/config.php
+++ application/config/config.php
@@ -35,7 +35,7 @@ $config['base_url'] = 'http://180.76.152.48/';
 | variable so that it is blank.
 |
 */
-$config['index_page'] = 'index.php';
+$config['index_page'] = '';
 
 /*
 |--------------------------------------------------------------------------
